4aeafb2914fd2289d1f65713c44e8cedeffedcd4
GnExia2 <ngai.garrick.j@gmail.com>
2020-06-02 15:13:43 -0700

    refactored controllers and routes into own folder

---
+++ controllers/userController.js
@@ -0,0 +1,30 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status:'error',
+    message:'This route is not yet defined for getting all users'
+  })
+}
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status:'error',
+    message:'This route is not yet defined for getting a user'
+  })
+}
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status:'error',
+    message:'This route is not yet defined for creating new users'
+  })
+}
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status:'error',
+    message:'This route is not yet defined for updating users'
+  })
+}
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status:'error',
+    message:'This route is not yet defined for deleting a user'
+  })
+}
