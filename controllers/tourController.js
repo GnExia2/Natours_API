287b3658aeb05d5c46b8998ce164354616e225e0
Garrick Ngai <garrickdark@yahoo.com>
2020-06-29 11:15:13 -0700

    created filter methods for API

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,8 +1,22 @@
 const Tour = require('./../models/tourModel')
 
 exports.getAllTours = async (req, res) => {
-try{
-    const tours = await Tour.find()
+  try{
+    console.log(req.query);
+
+    const queryObj = {...req.query}
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el])
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+
+
+
+    const query = await Tour.find(JSON.parse(queryStr));
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
@@ -12,7 +26,6 @@ try{
         tours
       }
     })
-
   }
   catch(err) {
     res.status(404).json({
